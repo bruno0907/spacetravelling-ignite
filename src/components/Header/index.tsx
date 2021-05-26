fc48d573f5da8c47fbec0caaf31b28df810e33a7
bruno0907 <bruno0907@gmail.com>
2021-05-26 18:03:25 -0300

    feat: added global styles. added header component. added font. added _document page. added header component to _app page.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.headerContent}>
+        <nav>
+          <Link href="/">
+            <a>
+              <img src="/logo.svg" alt="logo" />
+            </a>
+          </Link>
+        </nav>
+      </header>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,32 +5,38 @@ import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
 
-//   // TODO
-// };
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+export default function Home(): unknown {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(TODO);
+
+  return {
+    props: {},
+  };
+};
