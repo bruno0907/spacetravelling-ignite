0bebbf88110d6413f0550c1194324d785001100e
Bruno <bruno0907@gmail.com>
2022-01-06 13:11:06 -0300

    chore: remove of unused imports

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,6 @@
 import Prismic from '@prismicio/client';
 import { Document } from '@prismicio/client/types/documents';
 import { DefaultClient } from '@prismicio/client/types/client';
-import { ApiOptions } from '@prismicio/client/types/Api';
 
 const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
 const url = process.env.PRISMIC_API_ENTRY_POINT;
